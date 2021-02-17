93dc3807d3ce468672531e203f8f5a178cc4a0f4
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-02-16 22:01:04 -0300

    01 Fixing bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/global.css';
-import Index from './pages/Index/index'
+import './assets/style/global.css'
+import Index from './pages/Index/Index'
 
 ReactDOM.render(
   <React.StrictMode>
