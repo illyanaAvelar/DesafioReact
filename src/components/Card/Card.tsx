39184a48aac81158c3f2d8ea548820859a9ec768
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-02-19 10:08:07 -0300

    Fixing details button on group card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,34 +1,55 @@
 import React from "react";
 import * as C from "./styledComponents/index";
 import unoBasico1 from "./assets/uno-basico-1.0.png";
-import { Button } from "@material-ui/core";
+import { Button } from '../../shared/styledComponents/index'
+import { useState } from 'react';
+
+interface CardProps {
+  group: string;
+  type: string;
+  name: string;
+  details: object;
+}
 
 
-function Card() {
+function Card({group, type, name, details}: CardProps) {
+  const [showDetails, setShowDetails] = useState(false);
+  
+  const showDetailsOnClick = () => {
+    if(showDetails === true)
+    {
+      setShowDetails(false);
+    }
+    else
+    {
+      setShowDetails(true);
+    }
+  };
+  
   return (
     <C.Container>
       <C.Content>
         <C.TopCard>
           <p>
-            Grupo (A, B, C etc) - Tipo (Básico, Economico - Economico Premium -
-            Luxo) - Automatico/Manual
+            Grupo {group} - Tipo {type}
           </p>
           <img src={unoBasico1} alt="Task Icon" />
-          <h3> Nome do modelo = Fiat Uno 1.0</h3>
+          <h3> {name} </h3>
         </C.TopCard>
         <C.BottomCard>
           <h3>Descrição</h3>
           <p className="msg">Sua reserva garante um dos carros desse grupo.</p>
           <p className="msg">Modelo sujeito à disponibilidade da agência.</p>
-          <Button variant="outlined" color="primary"> Mais detalhes
+          <Button  
+          width={200}
+          negativeMargin={0}
+          onClick={showDetailsOnClick}> Mais detalhes
           </Button>
+              {showDetails && 
               <div id="maisDetalhes">
-                <p>4 Portas</p>
-                <p>4 Portas</p>
-                <p>4 Portas</p>
-                <p>4 Portas</p>
-                <p>4 Portas</p>
+                TESTE
               </div>
+              }
         </C.BottomCard>
       </C.Content>
     </C.Container>
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,11 +2,14 @@ import { CardListContainer } from './styledComponents/index'
 import Card from '../Card//Card'
 
 function CardList() {
+const object123 = {
+  obj1: 10
+}
   return (
    <CardListContainer>
-       <Card/>
-       <Card/>
-       <Card/>
+       <Card group='A' type="Tipo" name="Fiat Uno" details={object123}/>
+       <Card group='A' type="Tipo" name="Fiat Uno" details={object123}/>
+       <Card group='A' type="Tipo" name="Fiat Uno" details={object123}/>
    </CardListContainer>
   );
 }
