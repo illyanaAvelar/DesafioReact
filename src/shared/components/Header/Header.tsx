ed1a623c2b8ad6379168a0a7e15933b52f69e915
Illyana Guimarães de Avelar <illyana.guimaraes@dtidigital.com.br>
2021-02-19 17:01:22 -0300

    Adding return to home icon on header

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -3,28 +3,29 @@ import * as H from './styledComponents/index';
 import logo from "../../assets/images/whiteLogo.png";
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
+import Home from '@material-ui/icons/Home';
 
 function Header() {
   return (
     <H.Container>
       <H.LadoEsquerdo>
         <div id="ladoEsquerdo">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo"/>
         </div>
       </H.LadoEsquerdo>
-      <H.LadoDireito>
-         
-          <div id="minhasReservas">
-              <DirectionsCarIcon fontSize='large' style={{ color:'white', marginRight: 10 }}/>
-            <a href="bookings">Minhas Reservas</a>
+      <H.LadoDireito>      
+          <div id="perfil">
+              <Home fontSize='large' style={{ color:'white', marginRight: 10 }}/>  
+            <a href="/main">Home</a>
           </div>
-        
+          <div id="minhasReservas">
+              <DirectionsCarIcon fontSize='large' style={{ color:'white', marginRight: 10, marginLeft: 10 }}/>
+            <a href="bookings">Minhas Reservas</a>
+          </div>      
           <div id="perfil">
               <PersonRoundedIcon fontSize='large' style={{ color:'white', marginRight: 10 }}/>  
             <a href="/login">Entrar</a>
           </div>
-        
-      
         </H.LadoDireito>
     </H.Container>
   );
